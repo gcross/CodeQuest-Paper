0b8ca3e3e57178ae652808f744f7ca82a4046370
Gregory Crosswhite <gcross@phys.washington.edu>
2010-02-12 16:29:28 -0800

    Created mind map for tracking code results

---
+++ lattice-results/lattice-results.mm
@@ -0,0 +1,178 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1266016362462" ID="ID_424557317" MODIFIED="1266020907290" TEXT="Lattice Investigation Results">
+<node CREATED="1266016375432" ID="ID_1873800241" LINK="http://en.wikipedia.org/wiki/List_of_uniform_tilings" MODIFIED="1266020909539" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      11 Uniform Tilings
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1266018419725" FOLDED="true" ID="ID_664018952" MODIFIED="1266020907288">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Square
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1266020111827" ID="ID_143912608" MODIFIED="1266020150082">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="280px-Tiling_Regular_4-4_Square.svg.png" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1266020161283" FOLDED="true" ID="ID_253461438" MODIFIED="1266020907287" TEXT="Truncated square tiling (truncated quadrille)">
+<node CREATED="1266019272203" ID="ID_1028744106" MODIFIED="1266020171313">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="280px-Tiling_Semiregular_4-8-8_Truncated_Square.svg.png" />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1266020191715" FOLDED="true" ID="ID_1760082670" MODIFIED="1266020907287" TEXT="Snub square tiling (snub quadrille)">
+<node CREATED="1266019405185" ID="ID_777426162" MODIFIED="1266020196842">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="280px-Tiling_Semiregular_3-3-4-3-4_Snub_Square.svg.png" />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1266020222023" FOLDED="true" ID="ID_857725555" MODIFIED="1266020907287" TEXT="Hexagonal tiling (hextille)">
+<node CREATED="1266019500814" ID="ID_1581253976" MODIFIED="1266020219691">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="280px-Tiling_Regular_6-3_Hexagonal.svg.png" />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1266020238210" FOLDED="true" ID="ID_822151687" MODIFIED="1266020907286" TEXT="Trihexagonal tiling (hexadeltille)">
+<node CREATED="1266019544334" ID="ID_882421659" MODIFIED="1266020236665">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="280px-Tiling_Semiregular_3-6-3-6_Trihexagonal.svg.png" />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1266020317224" FOLDED="true" ID="ID_301892320" MODIFIED="1266020907285" TEXT="Truncated hexagonal tiling (truncated hextille)">
+<node CREATED="1266019705376" ID="ID_188143112" MODIFIED="1266020269849">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="280px-Tiling_Semiregular_3-12-12_Truncated_Hexagonal.svg.png" />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1266020330950" FOLDED="true" ID="ID_190100606" MODIFIED="1266020907285" TEXT="Triangular tiling (deltile)">
+<node CREATED="1266019738177" ID="ID_1815781030" MODIFIED="1266020329318">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="280px-Tiling_Regular_3-6_Triangular.svg.png" />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1266020342505" FOLDED="true" ID="ID_723089861" MODIFIED="1266020907285" TEXT="Rhombitrihexagonal tiling (rhombihexadeltille)">
+<node CREATED="1266019848115" ID="ID_1744503475" MODIFIED="1266020340641">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="280px-Tiling_Semiregular_3-4-6-4_Small_Rhombitrihexagonal.svg.png" />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1266020354441" FOLDED="true" ID="ID_801348014" MODIFIED="1266020907285" TEXT="Truncated trihexagonal tiling (truncated hexadeltille)">
+<node CREATED="1266019982755" ID="ID_1046639311" MODIFIED="1266020352947">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="280px-Tiling_Semiregular_4-6-12_Great_Rhombitrihexagonal.svg.png" />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1266020866477" FOLDED="true" ID="ID_1604045292" MODIFIED="1266020907284" TEXT="Snub hexagonal tiling (snub hexatille)">
+<node CREATED="1266020038915" ID="ID_1645343999" MODIFIED="1266020865437">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="280px-Tiling_Semiregular_3-3-3-3-6_Snub_Hexagonal.svg.png" />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1266020870311" FOLDED="true" ID="ID_1484100178" MODIFIED="1266020907284" TEXT="Elongated triangular tiling (isosnub quadrille)">
+<node CREATED="1266020897388" ID="ID_1199693642" MODIFIED="1266020901430">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="280px-Tiling_Semiregular_3-3-3-4-4_Elongated_Triangular.svg.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
